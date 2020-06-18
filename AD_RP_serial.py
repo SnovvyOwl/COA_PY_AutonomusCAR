0218eafacecd8042978a95f1f5968a69c5c34057
wonderland134 <50505387+wonderland134@users.noreply.github.com>
2020-06-18 21:08:07 +0900

    Add files via upload
    Arduino - raspberry pi serial communication

---
+++ AD_RP_serial.py
@@ -0,0 +1,104 @@
+import serial
+import time
+
+class Serial_command():
+    def __init__(self):
+        self.port = '/dev/ttyUSB0'
+        self.baud = 115200
+        self.timeout = 1
+        self.T_data = ""
+        self.T_data_history = ""
+        self.R_data = ""
+        self.ser = serial.Serial(self.port, self.baud, timeout = self.timeout)
+    
+    def Serial_check(self, timeout = 5):
+        i = 0
+        while True:
+            self.T_data = "@TEST#"
+            self.Serial_write()
+            time.sleep(1)
+            print('Waiting connection.')
+            self.Serial_read()
+            if self.R_data == "TEST":
+                break;
+            elif i == timeout :
+                print('Test fail');
+                break;
+            else:
+                i +=1
+    
+    def Serial_write(self):
+        if self.T_data != self.T_data_history or self.T_data =='@TEST#':
+            self.ser.write(self.T_data.encode("utf-8"))
+            self.T_data_history = self.T_data
+            self.T_data = ""
+    
+    def Serial_read(self):
+        Temp = self.ser.readline()[:-2].decode('utf-8')
+        if Temp:
+            self.R_data = Temp
+        else:
+            self.R_data = ""
+        
+    def Value_to_T_data(self,BF,LR):
+        BF_portion = ""
+        LR_portion = ""
+        
+        if BF >= 0:
+            BF_portion = "F" + str(BF)
+        else:
+            BF_portion = "B" + str(-BF)
+        
+        if LR >= 0:
+            LR_portion = "R" + str(LR)
+        else:
+            LR_portion = "L" + str(-LR)
+        
+        self.T_data = '@' + BF_portion + LR_portion + '#'
+
+class Position_status():
+    def __init__(self):
+        self.BF_desire = 20
+        self.LR_desire = 20
+        self.BF_current = 10
+        self.LR_current = 10
+        self.BF_pulse = 1
+        self.LR_pulse = 2
+    
+    def Set_BF_position(self, value):
+        self.BF_desire = value
+    
+    def Set_LR_position(self, value):
+        self.LR_desire = value
+    
+    def Change_BF_position(self):
+        if self.BF_desire > self.BF_current:
+            self.BF_current = self.BF_current + self.BF_pulse
+        if self.BF_desire < self.BF_current:
+            self.BF_current = self.BF_current - self.BF_pulse
+            
+    def Change_LR_position(self):
+        if self.LR_desire > self.LR_current:
+            self.LR_current = self.LR_current + self.LR_pulse
+        if self.LR_desire < self.LR_current:
+            self.LR_current = self.LR_current - self.LR_pulse
+
+if __name__ == '__main__':
+    MySerial = Serial_command()
+    MyPos = Position_status()
+
+    #Start serial connection
+    MySerial.Serial_check()
+
+    #Serial command
+    while True:
+        MyPos.Change_BF_position()
+        MyPos.Change_LR_position()
+        MySerial.Value_to_T_data(MyPos.BF_current, MyPos.LR_current)
+        if MySerial.T_data_history != MySerial.T_data:
+            print(MySerial.T_data)
+        MySerial.Serial_write()
+        MySerial.Serial_read()
+        if MySerial.R_data != "":
+            print(MySerial.R_data)
+        time.sleep(0.001)
