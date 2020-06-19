73c065b3b2f029871d1aa5e2e60fa882ae33407d
Oaflias <owl0614@naver.com>
2020-06-19 20:45:28 +0900

    run complete

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ with Raspberry Pi4
 
 step 1. Make RC Car, looks like Cyber Truck\
 step 2. Learn DL, openCV\
-step 3. Autonomus Car\
+step 3. Autonomus Car
--- SampleCode/socketserver.py
+++ SampleCode/socketserver.py
@@ -1,23 +1,22 @@
 import socket
-import time
 
 class Socketserver(object):
     def __init__(self):
-        self.host = "127.0.0.1" # HOST address
-        self.port = 4000 #port number
+        self.host = "0.0.0.0" # HOST address
+        self.port = 8080 #port number
         self.server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Socket Object is created 
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # AVOID port error 
         self.server_socket.bind((self.host,self.port)) #client Binding
-        self.server_socket.listen(1) # 1 Client is connectable
+        self.server_socket.listen() # 1 Client is connectable
         self.CMD = "None" # CMD
         self.client_socket, self.addr = self.server_socket.accept()#클라이언트 함수가 접속하면 새로운 소켓을 반환한다.
         self.status ="vel, angle" # Car's status
-        print("Controler is Connected")
+        print("Controler is Connected",self.addr)
     
     def receive_CMD(self):
         data=self.client_socket.recv(8) # 8Byte를 기다린다.
         self.CMD=data.encode()
-        self.client_socket.sendall(ength.to_bytes(8, byteorder="little")
+        self.client_socket.sendall(ength.to_bytes(8, byteorder="little"))
         self.client_socket.sendall(self.status)
     
     def connect_close(self):
