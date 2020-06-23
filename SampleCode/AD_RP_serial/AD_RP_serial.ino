cb01a857481693cb495d1aa7256a80a63ce72ed3
wonderland134 <50505387+wonderland134@users.noreply.github.com>
2020-06-23 17:33:29 +0900

    Update AD_RP_serial.ino
    Variable name changed

--- SampleCode/AD_RP_serial/AD_RP_serial.ino
+++ SampleCode/AD_RP_serial/AD_RP_serial.ino
@@ -3,11 +3,9 @@
 String R_data;
 String T_data;
 
-int BF_value = 0;
-int LR_value = 0;
-int LR_value_history = 0;
-
-bool flag = false;
+int BF_position = 0;
+int LR_position = 0;
+int LR_position_history = 0;
 
 int servo_min_pos = -500;
 int servo_max_pos = 500;
@@ -70,14 +68,14 @@ void Catch_value(String Command){
       value = value + Command[i];
       i++;
     }
-    BF_value = value.toInt();
+    BF_position = value.toInt();
   } else if(Command[i] == 'B'){
     i++;
     while ((Command[i] != 'L') && (Command[i] != 'R')){
       value = value + Command[i];
       i++;
     }
-    BF_value = value.toInt()*-1;
+    BF_position = value.toInt()*-1;
   }
   value = "";
   if (Command[i] == 'R'){
@@ -86,16 +84,16 @@ void Catch_value(String Command){
       value = value + Command[i];
       i++;
     }
-    LR_value_history = LR_value;
-    LR_value = value.toInt();
+    LR_position_history = LR_position;
+    LR_position = value.toInt();
   } else if (Command[i] == 'L'){
     i++;
     while (i < Command.length()){
       value = value + Command[i];
       i++;
     }
-    LR_value_history = LR_value;
-    LR_value = value.toInt()*-1;
+    LR_position_history = LR_position;
+    LR_position = value.toInt()*-1;
   }
 }
 
@@ -109,11 +107,12 @@ bool Serial_check(){
 }
 
 void servo_position(int val){
+  //Input data is servo_min_pos to servo_max_pos (probably -500 to 500 initially I set)
   //Servo speed : 0.17sec/60deg(0.0028333333sec/deg)
   val = map(val, servo_min_pos, servo_max_pos, servo_min_deg, servo_max_deg);
 
   //Calculate servo delay in milliseconds
-  float servo_delay = float(servo_max_deg-servo_min_deg)/float(servo_max_pos-servo_min_pos)*float(abs(LR_value-LR_value_history))*servo_speed*1000;
+  float servo_delay = float(servo_max_deg-servo_min_deg)/float(servo_max_pos-servo_min_pos)*float(abs(LR_position-LR_position_history))*servo_speed*1000;
   servo.write(val);
   delay(servo_delay);
 }
@@ -121,6 +120,7 @@ void servo_position(int val){
 void Start_setup(){
   //Check serial communication and servo position set 0
   servo_position(0);
+  bool flag = false;
   while (!flag){
     Serial_read();
     flag = Serial_check();
@@ -132,8 +132,8 @@ void loop() {
   Serial_read();
   if (R_data != "" && R_data != "TEST"){
     Catch_value(R_data);
-    servo_position(LR_value);
-    T_data = String(BF_value) + ", " + String(LR_value);
+    servo_position(LR_position);
+    T_data = String(BF_position) + ", " + String(LR_position);
     Serial_write(); 
   }
 }
