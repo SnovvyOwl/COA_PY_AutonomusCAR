f933bebfa52b9391b3fa940c9551dec8826f0e48
Oaflias <owl0614@naver.com>
2020-07-21 20:26:58 +0900

    test

--- HW_controller/camera.py
+++ HW_controller/camera.py
@@ -6,7 +6,7 @@ while(True):
 	ret, img_color=cap.read()
 	if ret == False:
 		break
-	img_gray=cv.cvtColor(img_color,cv.COLOR_BGR2GRAY_
+	img_gray=cv.cvtColor(img_color,cv.COLOR_BGR2GRAY)
 	cv.imshow('gray',img_gray)
 	if cv.waitKey(1) & 0xFF==27:
 		break
