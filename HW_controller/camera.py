8f2c625046bdef2de3b7abfba19e9009c842e57e
SnovvyOwl <owl0614@naver.com>
2020-07-21 20:01:36 +0900

    camera.py

---
+++ HW_controller/camera.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+cap=cv.VideoCapture(0)
+
+while(True):
+	ret, img_color=cap.read()
+	if ret == False:
+		break
+	img_gray=cv.cvtColor(img_color,cv.COLOR_BGR2GRAY_
+	cv.imshow('gray',img_gray)
+	if cv.waitKey(1) & 0xFF==27:
+		break
+		
+cap.release()
+cv.destroyAllWindow()
