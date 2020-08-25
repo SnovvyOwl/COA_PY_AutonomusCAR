c385bd43727a219e3e70eb66bc308adb85fac663
wonderland134 <50505387+wonderland134@users.noreply.github.com>
2020-08-25 20:27:44 +0900

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,10 @@
+class Car():
+    def __init__(self):
+        self.BF_desire = 100
+        self.LR_desire = 0
+    
+    def Set_BF_position(self, value):
+        self.BF_desire = value
+    
+    def Set_LR_position(self, value):
+        self.LR_desire = value
