759b1bd5936a3cb8c783936939daa0634743da8c
jinwook_choi <40587435+MeongJJeong@users.noreply.github.com>
2020-09-29 15:45:08 +0900

    200929
    python server
    + initializer overloading
    + change main method
    + add keyboardinterrupt

--- Server/server_python/2Server.py
+++ Server/server_python/2Server.py
@@ -1,29 +1,32 @@
 import socket
 from threading import Thread
 
-class SocketCommunication:
-    def __init__(self):
-        self.ip = "localhost"
-        self.port = 5005
-        self.clientNum = 5
 
+class SocketCommunication:
+    def __init__(self, ip=None, port=None):
         self.mSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if ip is None:
+            self.ip = "localhost"
+        if port is None:
+            self.port = 5005
+            return
+        self.ip = ip
+        self.port = port
 
     def startServer(self):
         print("[start server]")
         print("Server ip ->", self.ip)
         print("Server port->", self.port)
-        print("\n",self.clientNum, "can connectable this server")
+        # print("\n",, "can connectable this server")
 
         server_address = (self.ip, self.port)
         self.mSocket.bind(server_address)
-        self.mSocket.listen(self.clientNum)
+        self.mSocket.listen(2)
 
     def stopServer(self):
         print("[stop server]")
         print("- Thank you -")
         self.mSocket.close()
-
 
     def showInfo(self):
         print(self.ip, self.port)
@@ -43,11 +46,11 @@ class Client:
             while True:
                 data = self.connection.recv(1024)  # 8Byte를 기다린다.
                 CMD = data.decode()
-                print("controller: ",CMD)
+                print("controller: ", CMD)
 
         except ConnectionResetError:
             # self.thread.close()
-            print("disconnection from client")
+            print("disconnect from client")
 
         finally:
             self.connection.close()
@@ -58,35 +61,38 @@ class Client:
         self.connection.send(data.encode('utf-8'))
 
 
-class main:
+if __name__ == '__main__':
     receiverNum = 0
     controllerNum = 0
-
-    # receiver = []
-    # controller = []
 
     server = SocketCommunication()
     server.startServer()
 
-    while True:
-        # Wait for a connection
-        print("wait")
-        connection, client_address = server.mSocket.accept()
-        data = connection.recv(1024)  # 8Byte를 기다린다.
-        check = data.decode()           #속성 판단 receiver vs controller
+    try:
+        while KeyboardInterrupt:
+            # Wait for a connection
+            print("wait")
+            connection, client_address = server.mSocket.accept()
+            data = connection.recv(1024)  # 8Byte를 기다린다.
+            check = data.decode()  # 속성 판단 receiver vs controller
 
-        print("connection from", client_address)
+            print("connection from", client_address)
 
-        if check == "c":
-            print("controller is connected.")
-            thread = Thread(target= Client, args= (connection,client_address))
-            thread.start()
+            if check == "c":
+                print("controller is connected.")
+                thread = Thread(target=Client, args=(connection, client_address))
+                thread.start()
 
-        elif check == "r":
-            print("receiver is connected.")
-            thread = Thread(target=Client, args=(connection, client_address))
-            thread.start()
+            elif check == "r":
+                print("receiver is connected.")
+                thread = Thread(target=Client, args=(connection, client_address))
+                thread.start()
 
-        else:
-            print("Illegal connection has detected.")
-            connection.close()
+            else:
+                print("Illegal connection has detected.")
+                connection.close()
+
+    except KeyboardInterrupt:
+        server.stopServer()
+        print("이게 맞았으면 좋겠댱...")
+        pass
